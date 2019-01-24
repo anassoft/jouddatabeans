8a87d0d62482bac20665b49341fa8218c3461837
anas <anasalfasfous@yahoo.com>
2019-01-24 16:33:46 +0200

    Initial commit

---
+++ app/src/main/java/com/joud/admin/joudapp2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.joud.admin.joudapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.joud.admin.joudapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
